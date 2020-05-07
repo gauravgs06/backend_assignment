2221ed72da2f9728182a9794ba445fa394e35126
Gaurav Sharma <gauravsharmags06@gmail.com>
2020-05-07 10:31:19 +0530

    Fix Host Allow Error

--- BackendDevelopment/settings.py
+++ BackendDevelopment/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '6v2eny@w)7nn*609gqzfp#o2i#^_#4h8@!xrw3qiv$l@r93_iz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
